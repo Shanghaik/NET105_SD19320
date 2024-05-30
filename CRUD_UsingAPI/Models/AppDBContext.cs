1f2f5870ede77e68613191624ca13d600ec44919
shanghaik168@gmail.com <Shanghaik1998>
2024-05-30 19:06:36 +0700

    create db

---
+++ CRUD_UsingAPI/Models/AppDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_UsingAPI.Models
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext()
+        {
+        }
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=SHANGHAIK;Initial Catalog=CRUDAPI;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ CRUD_UsingAPI/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_UsingAPI.Models
+{
+    public class Student
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Email { get; set; }
+        public string  CCCD { get; set; }
+        public string Major { get; set; }
+
+    }
+}
