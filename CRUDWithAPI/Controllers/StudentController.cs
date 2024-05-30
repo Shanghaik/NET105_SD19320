63514d887d0b54410bbc6e185cbba38f14a8abe4
shanghaik168@gmail.com <Shanghaik1998>
2024-05-30 20:23:18 +0700

    CR

---
+++ CRUDWithAPI/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using CRUDWithAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CRUDWithAPI.Controllers
+{
+    public class StudentController : Controller
+    {
+        HttpClient client = new HttpClient();
+        public IActionResult GetAll()
+        {
+            // Lấy requestURL
+            string requestURL = @"https://localhost:7294/api/Student/get-all-student";
+            // Lấy response
+            var response = client.GetStringAsync(requestURL).Result;
+            List<Student> students = JsonConvert.DeserializeObject<List<Student>>(response);
+            return View(students);
+        }
+        public IActionResult Details(string id)
+        {
+            string requestURL = $@"https://localhost:7294/api/Student/get-by-id?id={id}";
+            // Lấy response
+            var response = client.GetStringAsync(requestURL).Result;
+            Student student = JsonConvert.DeserializeObject<Student>(response);
+            return View(student);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Student student = new Student() // tạo mới để thêm sẵn data sang view thôi nhé =))
+            {
+                Name = "Khánh đẹp trai",
+                Description = "Đã đẹp trai lại còn đấm đau",
+                Email = "cudamngancan@gmail.com",
+                Major = "Call Chicken"
+            };
+            return View(student);
+        }
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            string requestURL = "https://localhost:7294/api/Student/create-student";
+            // Lấy response
+            // var studentContent = JsonConvert.SerializeObject(student);  
+            var response = client.PostAsJsonAsync(requestURL, student).Result;
+            // Vid phương thức PostAsJsonAsync yêu cầu 2 tham số là requestURL và 1 chuỗi Json nên phải ép kiểu
+            return RedirectToAction("GetAll");
+        }
+    }
+}
---
+++ CRUDWithAPI/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUDWithAPI.Models
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Email { get; set; }
+        public string CCCD { get; set; }
+        public string Major { get; set; }
+    }
+}
